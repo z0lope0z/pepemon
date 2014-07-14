0a5fb1c48f8404e1fd9e6b435568d4e31ae55a01
Lope Emano <lopegwapo@gmail.com>
2014-07-14 19:03:36 +0800

    gradle migration with new License checker class

---
+++ app/src/main/java/com/lopefied/pepemon/playstore/License.java
@@ -0,0 +1,61 @@
+package com.lopefied.pepemon.playstore;
+
+/**
+ * Created by lope on 7/14/14.
+ */
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.RemoteException;
+
+class LicenseListener extends android.os.Binder {
+    static final String LISTENER = "com.android.vending.licensing.ILicenseResultListener";
+
+    public boolean onTransact(int op, Parcel in, Parcel reply, int flags) {
+        if (op == 1) {
+            in.enforceInterface(LISTENER);
+            int code = in.readInt();
+            String data = in.readString();
+            String signature = in.readString();
+            if (code == 0 || code == 2) {
+                // LICENSED or LICENSED_OLD_KEY
+            } else if (code == 1) {
+                // NOT_LICENSED
+            } else {
+                // ERROR
+            }
+        }
+        return true;
+    }
+}
+
+public class License {
+    static final String SERVICE = "com.android.vending.licensing.ILicensingService";
+
+    static void check(final Context context) {
+        context.bindService(
+                new Intent(SERVICE),
+                new ServiceConnection() {
+                    public void onServiceConnected(ComponentName name, IBinder binder) {
+                        Parcel d = Parcel.obtain();
+                        try {
+                            d.writeInterfaceToken("LICENSING_SERVICE");
+                            d.writeLong(0);
+                            d.writeString(context.getPackageName());
+                            d.writeStrongBinder(new LicenseListener());
+                            binder.transact(1, d, null, IBinder.FLAG_ONEWAY);
+                        } catch (RemoteException e) {
+                        }
+                        d.recycle();
+                    }
+
+                    public void onServiceDisconnected(ComponentName name) {
+                    }
+                },
+                Context.BIND_AUTO_CREATE);
+    }
+}
